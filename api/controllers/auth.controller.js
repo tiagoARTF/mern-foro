6e67fe83f975196118bc57b78b2256f6b3bedf00
Santiago Arteaga <102774495+tiagoARTF@users.noreply.github.com>
2023-10-17 10:50:54 -0500

    add sign out user functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -78,3 +78,7 @@ export const google = async (req, res, next) => {
       next(error);
     }
   };
+
+  export const signout = (req, res) => {
+    res.clearCookie('access_token').status(200).json('Has cerrado Sesión exitosamente!');
+  };
